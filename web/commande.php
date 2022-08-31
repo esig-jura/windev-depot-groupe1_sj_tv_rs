58c75436fb95ce4bc0f41b66359580a24c41dbf1
vuillaumetheo <theo.vuillaume@divcom.ch>
2022-08-31 11:57:14 +0200

    Affichage des plats dans l'application web

--- web/commande.php
+++ web/commande.php
@@ -7,6 +7,23 @@ if($_SESSION["logged"] != true) {
     header('Location : '.'./index.php');
 }
 
+require 'getpost/php-ref-master/ref.php';
+require 'conn.inc.php';
+
+//ouverture de la connexion
+$dbh = conn_db();
+
+//rédaction de la requête
+
+$sql = "SELECT * FROM ih1o2_pi02db1.tb_plat ORDER BY ih1o2_pi02db1.tb_plat.nom_plat;";
+
+// la compilation de la requête sur le serveur retour un objet PDOStatment qui représente la requête sur le serveur
+$stmt = $dbh->prepare($sql);
+
+//exécution de la requête
+$stmt->execute();
+
+$stmt->setFetchMode(PDO::FETCH_NUM);
 
 ?>
 
@@ -23,35 +40,69 @@ if($_SESSION["logged"] != true) {
     <h1>Liste des plats</h1>
 </header>
 <form action="https://lmo.divtec.me/pi02gr1/getpost/">
-    <div class="container">
-        <img class="plat" src="src/penne-pesto-avocat.jpg" alt="">
-        <div>
-            <div class="titre">
-                <h2>Spaghettis au pesto d'avocat</h2>
-                <img class="badge" src="src/vegan.png" alt="">
-                <img class="badge" src="src/bio.png" alt="">
-            </div>
-            <h3>Description</h3>
-            <p>The behavior could be thought of as a minimum gutter, as if the gutter is bigger somehow (because of something
-            like justify-content: space-between;) then the gap will only take effect if that space would end up smaller.</p>
-            <h3>Aliments</h3>
-            <p>Aliment : The behavior could be thought of as a minimum gutter, as if the gutter is bigger somehow (because of something</p>
-        </div>
-        <div class="prix-badge">
-            <div class="prix">
-                <h3>Prix : </h3>
-                <p>10.-</p>
-            </div>
-            <div class="quantite">
-                <label for="fqte"><h3>Quantité :</h3></label>
-                <input type="number" value="1" id="fqte" name="fqte">
-            </div>
-            <div class="prix-plat">
-                <h3>Prix total : </h3>
-                <p>10.-</p>
-            </div>
-        </div>
+    <?php
+    try {
+        foreach ($stmt as $row) {
+
+            echo '<div class="container">';
+            echo '<img class="plat" src="'.$row[7].'" alt="">';
+            echo ' <div><div class="titre">';
+            echo "<h2>$row[1]</h2>";
+
+            if ($row[4]){
+                echo '<img class="badge" src="src/bio.png" alt="">';
+            }
+
+            if ($row[5]){
+                echo '<img class="badge" src="src/vegan.png" alt="">';
+            }
+
+            echo '</div><h3>Description</h3>';
+            echo '<p>'.$row[2].'</p>';
+            echo ' <h3>Aliments</h3>';
+            echo '<p>liste des aliments</p></div>';
+            echo '<div class="prix-badge"><div class="prix"><h3>Prix : </h3>';
+            echo '<p>'.$row[3].'</p></div>';
+            echo ' <div class="quantite"><label for="fqte'.$row[0].'"><h3>Quantité :</h3></label>
+                  <input type="number" value="0" id="fqte'.$row[0].'" name="fqte'.$row[0].'"></div>';
+                   echo '<div class="prix-plat">
+            <h3>Prix total : </h3>
+            <p><span class="totalPlat'.$row[0].'"></span></p>
+            </div></div>
     </div>
+                  
+                                      <script>
+                    // Récupère la liste déroulante #pays et le span .code
+                    const quantitePlat'.$row[0].' = document.getElementById("fqte'.$row[0].'");
+                    const totalPlat'.$row[0].' = document.querySelector("span.totalPlat'.$row[0].'");
+                    
+                    // Sur changement de la valeur de la liste déroulante
+                    quantitePlat'.$row[0].'.addEventListener("change", function() {
+                       // Récupère la valeur de loption sélectionnée
+                       let total = quantitePlat'.$row[0].'.value;
+                       // Modifie le contenu texte du span .code
+                       let totalPrix = total*'.$row[3].';
+                       
+                       if (totalPrix < 1){
+                       
+                       totalPrix = 0;
+                       } 
+                       
+                       totalPlat' . $row[0] . '.innerText = totalPrix;
+                    });
+                            </script>
+
+
+                  ';
+        
+
+
+        }
+
+    } catch (PDOException $e) {
+        die($e->getMessage());
+    }
+    ?>
     <button type="submit">Commander</button>
 </form>
 <footer>
@@ -60,3 +111,4 @@ if($_SESSION["logged"] != true) {
 </footer>
 </body>
 </html>
+
