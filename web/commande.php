8b74043e4c4d7ee14ac047c5ed4de5777e270e36
vuillaumetheo <theo.vuillaume@divcom.ch>
2022-09-02 16:49:39 +0200

    Formulaire commande fonctionnel

--- web/commande.php
+++ web/commande.php
@@ -1,11 +1,11 @@
 <?php
 
 session_start();
-/*
+
 if($_SESSION["logged"] != true) {
 //echo 'not logged in';
     header('Location : '.'./index.php');
-}*/
+}
 
 require 'getpost/php-ref-master/ref.php';
 require 'conn.inc.php';
@@ -60,7 +60,38 @@ $stmt->setFetchMode(PDO::FETCH_NUM);
             echo '</div><h3>Description</h3>';
             echo '<p>'.$row[2].'</p>';
             echo ' <h3>Aliments</h3>';
-            echo '<p>liste des aliments</p></div>';
+
+
+            //rédaction de la requête
+
+            $sql = "SELECT tb_aliment.nom_alim FROM tb_aliment
+                     INNER JOIN tb_composer ON tb_aliment.id_alim = tb_composer.fk_comp_alim
+                     INNER JOIN tb_plat ON tb_composer.fk_comp_plat = tb_plat.id_plat 
+                     WHERE tb_plat.id_plat = $row[0];";
+
+            // la compilation de la requête sur le serveur retour un objet PDOStatment qui représente la requête sur le serveur
+            $stmt = $dbh->prepare($sql);
+
+            //exécution de la requête
+            $stmt->execute();
+
+            $stmt->setFetchMode(PDO::FETCH_NUM);
+
+            $liste_aliments = "";
+            foreach ($stmt as $aliment) {
+
+
+
+                if ($liste_aliments != ""){
+                    $liste_aliments = $liste_aliments.", ".$aliment[0];
+                } else {
+                    $liste_aliments = $liste_aliments.$aliment[0];
+                }
+
+
+            }
+
+            echo '<p>'.$liste_aliments.'</p></div>';
             echo '<div class="prix-badge"><div class="prix"><h3>Prix : </h3>';
             echo '<p>'.$row[3].'</p></div>';
             echo ' <input id="idplat'.$row[0].'" name="idplat'.$row[0].'" type="hidden" value="idplat'.$row[0].'">
@@ -69,15 +100,19 @@ $stmt->setFetchMode(PDO::FETCH_NUM);
                    echo '<div class="prix-plat">
             <h3>Prix total : </h3>
             <p><span class="totalPlat'.$row[0].'"></span></p>
+            <input id="prix-total-plat" class="prix-total-plat'.$row[0].'" name="input-prix-total-plat" type="hidden" value="0">
             </div></div>
     </div>
                   
-                                      <script>
-                    // Récupère la liste déroulante #pays et le span .code
+               <script>
+                    // Récupération de la quantité du plat
                     const quantitePlat'.$row[0].' = document.getElementById("fqte'.$row[0].'");
+                    const totalPrixPlat'.$row[0].' = document.querySelector("input.prix-total-plat'.$row[0].'");
+                    
+                    // Span qui affichera le montant total du plat
                     const totalPlat'.$row[0].' = document.querySelector("span.totalPlat'.$row[0].'");
                     
-                    // Sur changement de la valeur de la liste déroulante
+                    // Sur changement de la valeur de linput de quantité
                     quantitePlat'.$row[0].'.addEventListener("change", function() {
                        // Récupère la valeur de loption sélectionnée
                        let total = quantitePlat'.$row[0].'.value;
@@ -87,40 +122,41 @@ $stmt->setFetchMode(PDO::FETCH_NUM);
                        if (totalPrix < 1){
                        
                        totalPrix = 0;
-                       } 
+                       }; 
                        
                        totalPlat' . $row[0] . '.innerText = totalPrix;
+                       totalPrixPlat' . $row[0] . '.value = totalPrix;
                     });
                             </script>
-
-
-                  ';
-        
-
-
-        }
+                  ';}
 
     } catch (PDOException $e) {
         die($e->getMessage());
     }
     ?>
+    <div class="prix-total">
     <h3>Prix total de la commande : </h3>
     <p><span class="totalCommande"></span></p>
+    </div>
     <script type="text/javascript">
 
         function findTotal(){
 
             const totalCommande= document.querySelector("span.totalCommande");
             const quantitePlat = document.getElementsByClassName('input-qte');
-            const prixPlat = document.getElementsByClassName('totalPlat*');
+            const prixPlat = document.getElementsByName('input-prix-total-plat');
+
+
 
                 var arr = quantitePlat;
                 var tot=0;
 
                 for(var i=0;i<arr.length;i++){
 
+                    console.info(prixPlat[i].value);
+
                     if(parseInt(arr[i].value))
-                        tot += parseInt(arr[i].value);
+                        tot += parseInt(prixPlat[i].value);
                 }
                 totalCommande.innerText = tot;
 
--- web/getpost/index.php
+++ web/getpost/index.php
@@ -1,120 +1,200 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: Théo Vuillaume
+ * Date: 30.08.2022
+ * Time: 09:00
+ */
+
 session_start();
 $_SESSION['logged'] = false;
 
-require 'getpost/php-ref-master/ref.php';
-require 'conn.inc.php';
-
-//ouverture de la connexion
-$dbh = conn_db();
-
-//rédaction de la requête
-
-$sql = "SELECT * FROM ih1o2_pi02db1.tb_localite ORDER BY ih1o2_pi02db1.tb_localite.npa_loc;";
-
-// la compilation de la requête sur le serveur retour un objet PDOStatment qui représente la requête sur le serveur
-$stmt = $dbh->prepare($sql);
-
-//exécution de la requête
-$stmt->execute();
-
-$stmt->setFetchMode(PDO::FETCH_NUM);
-
-?>
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulaires</title>
-    <link rel="stylesheet" href="css/login-inscription.css">
-</head>
-<body>
-<div class="background">
-    <div class="container" id="container">
-        <div class="form-container sign-up-container">
-            <form action="https://lmo.divtec.me/pi02gr1/getpost/" method="post">
-                <h1>Inscription</h1>
-                <input type="email" name="f-ins-email" placeholder="Email"/>
-                <input type="text" name="f-ins-nom" placeholder="Nom"/>
-                <input type="text" name="f-ins-pre" placeholder="Prénom"/>
-                <input type="text" name="f-ins-adre" placeholder="Adresse"/>
-                <select id="f-ins-npa" name="f-ins-loc">
-                    <libellé>NPA</libellé>
-                    <?php
-                    try {
-                        foreach ($stmt as $row) {
-                            echo'<option value="'.$row[0].'">'.$row[1].' '.$row[2].'</option>';
-                        }
-                    } catch (PDOException $e) {
-                        die($e->getMessage());
-                    }
-                    ?>
-                </select>
-                <input type="password" name="f-ins-mdp" placeholder="Mot de passe"/>
-                <button type="submit" name="inscription" >Inscription</button>
-            </form>
-        </div>
-        <div class="form-container sign-in-container">
-            <form action="https://lmo.divtec.me/pi02gr1/getpost/" method="post">
-                <h1>Connexion</h1>
-                <input type="email" name="f-con-email" id="f-con-email" placeholder="Email"/>
-                <input type="password" name="f-con-mdp" placeholder="Mot de passe" />
-                <a href="#">Mot de passe oublié ?</a>
-                <button type="submit" name="connexion">Connexion</button>
-            </form>
-        </div>
-        <div class="overlay-container">
-            <div class="overlay">
-                <div class="overlay-panel overlay-left">
-
-                    <h1>Bentornati!</h1>
-                    <p>Tagliatelle à la truffe ? N'attends pas, connecte toi </p>
-                    <button class="ghost" id="signIn">Connexion</button>
-                </div>
-                <div class="overlay-panel overlay-right">
-                    <h1>Un petit creux ?</h1>
-                    <p>Inscris toi pour déguster nos superbes pâtes</p>
-                    <button class="ghost" id="signUp">Inscription</button>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<div class="form-responsive">
-    <form action="https://lmo.divtec.me/pi02gr1/getpost/" method="post">
-        <h1>Connexion</h1>
-        <input type="email" name="f-con-email" id="f-con-email" placeholder="Email"/>
-        <input type="password" name="f-con-mdp" placeholder="Mot de passe" />
-        <a href="#">Mot de passe oublié ?</a>
-        <button type="submit" name="connexion">Connexion</button>
-    </form>
-    <form action="https://lmo.divtec.me/pi02gr1/getpost/" method="post">
-        <h1>Inscription</h1>
-        <input type="email" name="f-ins-email" placeholder="Email"/>
-        <input type="text" name="f-ins-nom" placeholder="Nom"/>
-        <input type="text" name="f-ins-pre" placeholder="Prénom"/>
-        <input type="text" name="f-ins-adre" placeholder="Adresse"/>
-        <input type="number" name="f-ins-npa" placeholder="NPA"/>
-        <input type="text" name="f-ins-loc" placeholder="Localité"/>
-        <input type="password" name="f-ins-mdp" placeholder="Mot de passe"/>
-        <button type="submit" name="inscription" >Inscription</button>
-    </form>
-</div>
-</body>
-</html>
-<script>const signUpButton = document.getElementById('signUp');
-    const signInButton = document.getElementById('signIn');
-    const container = document.getElementById('container');
-
-    signUpButton.addEventListener('click', () => {
-        container.classList.add("right-panel-active");
-    });
-
-    signInButton.addEventListener('click', () => {
-        container.classList.remove("right-panel-active");
-    });
 
 
+require 'php-ref-master/ref.php';
+require '../conn.inc.php';
 
-</script>
+// Vérifier si le formulaire est soumis
+if (isset($_POST['inscription'])) {
+
+    $nom = filter_input(INPUT_POST, 'f-ins-nom', FILTER_SANITIZE_STRING);
+    $prenom = filter_input(INPUT_POST, 'f-ins-pre', FILTER_SANITIZE_STRING);
+    $adresse = filter_input(INPUT_POST, 'f-ins-adre', FILTER_SANITIZE_STRING);
+    $email = filter_input(INPUT_POST, 'f-ins-email', FILTER_SANITIZE_EMAIL);
+    $mdp = filter_input(INPUT_POST, 'f-ins-mdp', FILTER_SANITIZE_STRING);
+    $loc = filter_input(INPUT_POST, 'f-ins-loc', FILTER_SANITIZE_NUMBER_INT);
+
+    try {
+
+        //ouverture de la connexion
+        $dbh = conn_db();
+
+        //rédaction de la requête
+
+        $sql = "insert into tb_utilisateur values(null,:nom,:pre,:adr,:email,:mdp,0,:loc)";
+
+        // la compilation de la requête sur le serveur retour un objet PDOStatment qui représente la requête sur le serveur
+        $stmt = $dbh->prepare($sql);
+
+        //association du marqueur à une variable (E/S)
+        //lier les données
+        $stmt->bindParam(':nom', $nom, PDO::PARAM_STR);
+        $stmt->bindParam(':pre', $prenom, PDO::PARAM_STR);
+        $stmt->bindParam(':adr', $adresse, PDO::PARAM_STR);
+        $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+        $stmt->bindParam(':mdp', $mdp, PDO::PARAM_STR);
+        $stmt->bindParam(':loc', $loc, PDO::PARAM_INT);
+
+        //exécution de la requête
+        $stmt->execute();
+
+        //choix du mode de récuperation
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+        // récupère le nombre d'enregistrement affectés par la requête
+        $res = $stmt->rowCount();
+
+        //redirection vers la page d'accueil
+        // header('location:index.html');
+
+    } catch (PDOException $e) {
+        die($e->getMessage());
+    }
+    
+    header('Location : '.'../commande.php');
+
+
+} elseif (isset($_POST['connexion'])){
+
+    $email = filter_input(INPUT_POST, 'f-con-email', FILTER_SANITIZE_STRING);
+    $mdp = filter_input(INPUT_POST, 'f-con-mdp', FILTER_SANITIZE_STRING);
+
+    //ouverture de la connexion
+    $dbh = conn_db();
+
+    //rédaction de la requête
+
+    $sql = "SELECT email_util, mdp_util, id_util FROM ih1o2_pi02db1.tb_utilisateur where email_util LIKE :email";
+
+    // la compilation de la requête sur le serveur retour un objet PDOStatment qui représente la requête sur le serveur
+    $stmt = $dbh->prepare($sql);
+
+    //association du marqueur à une variable (E/S)
+    //lier les données
+    $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+
+    //exécution de la requête
+    $stmt->execute();
+
+    $stmt->setFetchMode(PDO::FETCH_NUM);
+
+    $mdp_util = "";
+    foreach ($stmt as $row) {
+        $mdp_util = $row[1];
+    }
+
+
+    if ($mdp_util == $mdp){
+
+        $_SESSION['logged'] = true;
+        $_SESSION['id'] = $row[2];
+        echo "Logged in";
+        header('Location : '.'../commande.php');
+    } else {
+        header('Location : '.'../index.php');
+    }
+}elseif (isset($_GET)){
+
+    try {
+
+      //  var_dump($_GET);
+
+
+        //ouverture de la connexion
+        $dbh = conn_db();
+
+        //rédaction de la requête
+
+        $dateHeure = date('y-m-d h:i:s');
+
+        $iduser = $_SESSION["id"];
+
+        $sql = "insert into tb_commande values(null,0,'$dateHeure','$iduser')";
+
+        // la compilation de la requête sur le serveur retour un objet PDOStatment qui représente la requête sur le serveur
+        $stmt = $dbh->prepare($sql);
+
+        //exécution de la requête
+        $stmt->execute();
+
+        //choix du mode de récuperation
+        $stmt->setFetchMode(PDO::FETCH_NUM);
+
+
+        $id_plat = -1;
+        $qte_plat = -1;
+        $id_com = $dbh->lastInsertId();
+
+        unset($_GET['input-prix-total-plat']);
+
+        foreach ($_GET as $plat) {
+
+            //var_dump($plat);
+
+            if (strpos($plat, "id") !== false){
+                $id_plat = substr($plat, -2);
+            } else {
+                $qte_plat = $plat;
+            }
+
+            echo 'QTE : ';
+
+
+
+
+            if ($qte_plat ==-1){
+                continue;
+            }
+
+            if ($qte_plat ==0){
+                $id_plat = -1;
+                $qte_plat = -1;
+                continue;
+            }
+
+            $sql = "insert into tb_contenir values(null,:fk_cont_plat,:fk_cont_com,:qte_cont)";
+
+            // la compilation de la requête sur le serveur retour un objet PDOStatment qui représente la requête sur le serveur
+            $stmt = $dbh->prepare($sql);
+
+            //association du marqueur à une variable (E/S)
+            //lier les données
+
+            $stmt->bindParam(':fk_cont_plat', $id_plat, PDO::PARAM_INT);
+            $stmt->bindParam(':fk_cont_com', $id_com, PDO::PARAM_INT);
+            $stmt->bindParam(':qte_cont', $qte_plat, PDO::PARAM_INT);
+
+           // echo 'QTE : ';
+          //  var_dump($qte_plat);
+
+            //exécution de la requête
+            $stmt->execute();
+
+           // var_dump($stmt);
+
+
+
+
+
+            $id_plat = -1;
+            $qte_plat = -1;
+
+        }
+
+    } catch (PDOException $e) {
+        die($e->getMessage());
+    }
+    $_SESSION['logged'] = true;
+    header('Location : '.'../commande.php');
+
+}
